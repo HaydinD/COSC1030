37b88e083534365e248d1dd777570d3bd02b771d
HaydinD <haydindavis@crb1.net>
2025-01-23 09:43:39 -0700

    Update main.cpp

--- hello-world/main.cpp
+++ hello-world/main.cpp
@@ -1,7 +1,14 @@
 //
-// Name
-// Date
-// Hello World Programming Project
+// Haydin Davis
+// 1/23
+// Your First C++ Program
 // COSC 1030
 //
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!";
+    return 0;
+}

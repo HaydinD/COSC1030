692927d152bdcd222ecb0f3c76e8ddae248cf17e
HaydinD <haydindavis@crb1.net>
2025-01-28 09:42:50 -0700

    Update main.cpp

--- ocean-levels/main.cpp
+++ ocean-levels/main.cpp
@@ -1,8 +1,18 @@
 //
-// Name
-// Date
+// Haydin Davis
+// 1/28
 // Ocean Levels Programming Project
 // COSC 1030
 //
+#include <iostream>
+using namespace std;
 
-
+int main()
+{
+ 	cout << "After 5 years the sea level will be "
+		<< 5+1.5 << "mm higher." << endl;
+		cout << "After 7 years the sea level will be "
+		<< 7+1.5 << "mm higher." << endl;
+		cout << "After 10 years the sea level will be "
+		<< 10+1.5 << "mm higher." << endl;
+}

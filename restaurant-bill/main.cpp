406e9541ec61a118832a3e9faccd4a140a242b67
HaydinD <haydindavis@crb1.net>
2025-01-28 10:04:22 -0700

    Update main.cpp

--- restaurant-bill/main.cpp
+++ restaurant-bill/main.cpp
@@ -1,20 +1,33 @@
 //
-// Name
-// Date
+// Haydin Davis
+// 1/28
 // Restaurant Bill Programming Project
 // COSC 1030
 //
+#include<iostream>
+using namespace std;
 
-// Variable declarations
-
-// Constants for the tax rate and tip percentage
-
-// Get the amount of the purchase.
-
-// Calculate the sales tax.
-
-// Calculate the tip.
-
-// Calculate the total of the sale.
-
-// Print information about the sale.
+int main()
+{
+    // Variable declarations
+  
+    // Constants for the tax rate and tip percentage
+    const float TAX=0.0675;
+    const float TIP=0.15;
+    // Get the amount of the purchase.
+    double meal=88.67;
+    
+    // Calculate the sales tax.
+    double tax=meal*TAX;
+    // Calculate the tip.
+    double subtotal= meal+tax;
+    double tip=subtotal*TIP;
+    // Calculate the total of the sale.
+    double total=tip+subtotal;
+    // Print information about the sale.
+    cout<<"Meal cost: "<<meal<<endl;
+    cout<<"Tax: "<<tax<<endl;
+    cout<<"Tip: "<<tip<<endl;
+    cout<<"Total: "<<total<<endl;
+    return 0;
+}

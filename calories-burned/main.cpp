36ae3c4b323e319ff745a2480cc21fa9f4d9ad32
HaydinD <haydindavis@crb1.net>
2025-02-18 10:15:15 -0700

    Update main.cpp

--- calories-burned/main.cpp
+++ calories-burned/main.cpp
@@ -1,7 +1,26 @@
 //
-// Name
-// Date
+// Haydin Davis
+// 2/18
 // Calories Burned Programming Project
 // COSC 1030
 //
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    const double CALS_PER_MIN=3.9;
+    
+    int minutes;
+    double calories;
+    
+    for(minutes=10; minutes<=30; minutes+=5)
+    {
+        calories=minutes*CALS_PER_MIN;
+        cout<<"After "<<minutes<<" minutes you burned "
+        <<calories<<" calories.\n";
+    }
+		
+    return 0;
+}
 

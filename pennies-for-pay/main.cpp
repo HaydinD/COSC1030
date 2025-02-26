9d5fdb5063e19f1e96ec04a63c5e0226f1217eba
HaydinD <haydindavis@crb1.net>
2025-02-26 10:07:41 -0700

    Update main.cpp

--- pennies-for-pay/main.cpp
+++ pennies-for-pay/main.cpp
@@ -1,8 +1,31 @@
 //
-// Name
-// Date
+// Haydin Davis
+// 2/26
 // Pennies for Pay Programming Project
 // COSC 1030
 //
+#include<iostream>
+using namespace std;
 
-
+int main()
+{
+    int mult=1;
+    int i=1;
+    float total=0;
+    float salary=0.01;
+    int days;
+    cout<<"How many days have you worked: ";
+    cin>>days ;
+    if(days>=1){
+        while(i<=days){
+           cout<<mult<<"*"<<salary<<"= $"<<mult*salary<<endl;
+           total+=mult*salary;
+           i+=1;
+           mult*=2;
+        }
+        cout<<"You worked "<<days<<" days for a total of "<<total<<" dollars."<<endl;
+    }
+            
+		
+    return 0;
+}

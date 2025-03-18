ffb95c563d5261d36b957d929da40eee42d2ce23
HaydinD <haydindavis@crb1.net>
2025-03-18 10:20:32 -0600

    Update main.cpp

--- markup/main.cpp
+++ markup/main.cpp
@@ -4,5 +4,33 @@
 // Markup Programming Project
 // COSC 1030
 //
+#include <iostream>
+#include<iomanip>
+using namespace std;
 
+double calculateRetail(double,double);
+
+int main() {
+    double wholesale;
+    double markupPercent;
+    double retail;
+    
+    cout<<"Enter the items wholesale cost: ";
+    cin>>wholesale;
+    
+    cout<<"Enter the items markup percent: ";
+    cin>>markupPercent;
+    
+    retail= calculateRetail(wholesale,markupPercent);
+    
+    cout<<"The retail price is $"<<setprecision(2)
+    <<fixed<<retail<<endl;
+    
+    return 0;
+}
+
+double calculateRetail(double wholesale,double markupPercent){
+    double markup=wholesale*markupPercent/100;
+    return wholesale+markup;
+}
 

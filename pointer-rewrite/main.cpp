ccdd01115f931dd64a0c9db433fb89065ab88493
HaydinD <haydindavis@crb1.net>
2025-04-17 09:37:02 -0600

    Update main.cpp

--- pointer-rewrite/main.cpp
+++ pointer-rewrite/main.cpp
@@ -1,7 +1,34 @@
 //
-// Name
-// Date
+// Haydin Davis
+// 4/17
 // Pointer Rewrite Programming Project
 // COSC 1030
 //
+#include<iostream>
+using namespace std;
 
+int doSomething(int&,int&);
+
+int main()
+{
+    int x=10, y=2;
+    cout<<"Before calling the function:\n"
+    <<"x= "<<x<<endl
+    <<"y= "<<y<<endl<<endl;
+    
+    cout<<"The function returns "
+    <<doSomething(x,y)<<endl<<endl;
+    
+    cout<<"After calling the function:\n"
+    <<"x= "<<x<<endl
+    <<"y= "<<y<<endl<<endl;
+    
+    return 0;
+}
+int doSomething(int&x,int&y)
+{
+    int temp=x;
+    x=y*10;
+    y=temp*10;
+    return x+y;
+}
